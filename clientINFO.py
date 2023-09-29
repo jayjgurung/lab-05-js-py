c926003389aaff18f3c77244c31de583d30618ef
jayjgurung <newuser@Jxxded-MBP.lan1>
2023-09-29 05:59:37 -0400

    Added a python program that collects customer info and displays them

---
+++ clientINFO.py
@@ -0,0 +1,8 @@
+clientName = input("Name:")
+clientAddress= input("Street address:")
+clientZipCode = input("Zip Code:")
+clientPhoneNumber = input("Phone Number:")
+clientEmail = input("Email Address:")
+clientDOB = input("Date of birth:")
+
+print("\t"+clientName+"Profile \nName : "+clientName+"\nAddress : "+clientAddress+ "\nZip Code : "+clientZipCode+ "\nPhone Number : "+clientPhoneNumber+"\nEmail : "+clientEmail+"\nDate of Birth : "+clientDOB)

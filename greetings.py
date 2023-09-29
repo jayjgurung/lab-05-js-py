3c769e81cf6a03431becb2246eb04d59e8819367
jayjgurung <newuser@Jxxded-MBP.lan1>
2023-09-28 20:39:08 -0400

    Updated the directory with .html and .py files

---
+++ greetings.py
@@ -0,0 +1,5 @@
+# no body, head or script opening or closing required
+
+print("Welcome to my first JavaScript Program")
+username = input("What's your name?")
+print("Hi "+ username + "!" + " Welcome, Once again.") // displays 
---
+++ madlibs.py
@@ -0,0 +1,8 @@
+
+print("Welcome to Madlibs Session")
+myNoun = input("Enter a noun:")
+myAdjective = input("Enter an adjective:")
+myAdverb = input("Enter an adverb:")
+myVrb = input("Enter a verb:")
+#display
+print("On a "+myAdverb+" "+myAdjective+" island, \nUnderneath a beautiful "+myNoun+".\n"+myVerb+" with the hula dancers, \nAskin' questions cause' they got all the answers.")

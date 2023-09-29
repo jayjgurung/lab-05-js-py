8682d09384ebd0438881c83b0399949b7b4fbc75
jayjgurung <newuser@Jxxded-MacBook-Pro.local>
2023-09-29 04:14:41 -0400

    Updated madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -3,6 +3,6 @@ print("Welcome to Madlibs Session")
 myNoun = input("Enter a noun:")
 myAdjective = input("Enter an adjective:")
 myAdverb = input("Enter an adverb:")
-myVrb = input("Enter a verb:")
+myVerb = input("Enter a verb:")
 #display
 print("On a "+myAdverb+" "+myAdjective+" island, \nUnderneath a beautiful "+myNoun+".\n"+myVerb+" with the hula dancers, \nAskin' questions cause' they got all the answers.")
